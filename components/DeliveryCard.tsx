8444f4b9e440522bd0ecaa242406b6114a70daf7
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-11-26 20:49:08 -0800

    DeliveryCard created

---
+++ components/DeliveryCard.tsx
@@ -0,0 +1,74 @@
+import { Card, Divider, Icon } from '@rneui/themed';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { useTailwind } from 'tailwind-rn/dist';
+
+type Props = {
+	order: Order;
+};
+
+const DeliveryCard = ({ order }: Props) => {
+	const tw = useTailwind();
+
+	return (
+		<Card
+			containerStyle={[
+				tw('rounded-lg my-2'),
+				{
+					backgroundColor: '#59C1CC',
+					padding: 0,
+					paddingTop: 16,
+					shadowColor: 'black',
+					shadowOffset: { width: 0, height: 2 },
+					shadowOpacity: 0.2,
+					shadowRadius: 4,
+				},
+			]}
+		>
+			<View>
+				<Icon name="box" type="entypo" color="white" size={50} />
+
+				<View>
+					<Text
+						style={tw('text-xs text-center uppercase text-white font-bold')}
+					>
+						{order.carrier} - {order.trackingId}
+					</Text>
+
+					<Text style={tw('text-white text-center text-lg font-bold')}>
+						Expected Delivery: {new Date(order.createdAt).toLocaleDateString()}
+					</Text>
+
+					<Divider color="white" style={tw('mt-4')} />
+				</View>
+
+				<View style={tw('mx-auto')}>
+					<Text style={tw('text-center text-white font-bold mt-5 text-base')}>
+						Address
+					</Text>
+
+					<Text style={tw('text-sm text-center text-white')}>
+						{order.Address}, {order.City}
+					</Text>
+
+					<Text style={tw('text-sm text-center italic text-white')}>
+						Shipping Cost: ${order.shippingCost}
+					</Text>
+				</View>
+
+				<Divider color="white" style={tw('mt-6')} />
+
+				<View style={tw('p-5')}>
+					{order.trackingItems.items.map((item) => (
+						<View style={tw('flex-row justify-between items-center')}>
+							<Text style={tw('text-sm italic text-white')}>{item.name}</Text>
+							<Text style={tw('text-white text-xl')}>x {item.quantity}</Text>
+						</View>
+					))}
+				</View>
+			</View>
+		</Card>
+	);
+};
+
+export default DeliveryCard;
--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,10 +1,62 @@
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import {
+	CompositeNavigationProp,
+	RouteProp,
+	useNavigation,
+	useRoute,
+} from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Icon } from '@rneui/themed';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { useTailwind } from 'tailwind-rn/dist';
+import DeliveryCard from '../components/DeliveryCard';
+import useCustomerOrders from '../hooks/useCustomerOrders';
+import { RootStackParamList } from '../navigator/RootNavigator';
+import { TabStackParamList } from '../navigator/TabNavigator';
+
+type ModalScreenNavigationProp = CompositeNavigationProp<
+	BottomTabNavigationProp<TabStackParamList>,
+	NativeStackNavigationProp<RootStackParamList, 'Modal'>
+>;
+
+type ModalScreenRouteProp = RouteProp<RootStackParamList, 'Modal'>;
 
 const ModalScreen = () => {
+	const tw = useTailwind();
+	const navigation = useNavigation();
+	const {
+		params: { name, userId },
+	} = useRoute<ModalScreenRouteProp>();
+
+	const { loading, error, orders } = useCustomerOrders(userId);
+
 	return (
 		<View>
-			<Text>ModalScreen</Text>
+			<TouchableOpacity
+				onPress={navigation.goBack}
+				style={tw('absolute right-5 top-5 z-10 mt-10')}
+			>
+				<Icon name="closecircle" type="antdesign" />
+			</TouchableOpacity>
+
+			<View style={tw('mt-4')}>
+				<View style={[tw('py-5 border-b'), { borderColor: '#59C1CC' }]}>
+					<Text
+						style={[tw('text-center text-xl font-bold'), { color: '#59C1CC' }]}
+					>
+						{name}
+					</Text>
+					<Text style={[tw('text-center italic text-sm')]}>deliveries</Text>
+				</View>
+			</View>
+
+			<FlatList
+				data={orders}
+				keyExtractor={(order) => order.trackingId}
+				renderItem={({ item: order }) => <DeliveryCard order={order} />}
+				contentContainerStyle={{ paddingBottom: 200 }}
+			/>
 		</View>
 	);
 };
