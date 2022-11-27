b7fd37b64d53ec9f1f53e46ea2967b6371e6f9a4
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-11-26 19:10:35 -0800

    Apollo GraphQL Client implemented

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
 import { TailwindProvider } from 'tailwind-rn/dist';
 import RootNavigator from './navigator/RootNavigator';
 import utilities from './tailwind.json';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:5001/api/deliveries',
+	cache: new InMemoryCache(),
+});
+
 export default function App() {
 	return (
+		// @ts-ignore - TailwindProvier is missing a type definition
 		<TailwindProvider utilities={utilities}>
-			<NavigationContainer>
-				<RootNavigator />
-			</NavigationContainer>
+			<ApolloProvider client={client}>
+				<NavigationContainer>
+					<RootNavigator />
+				</NavigationContainer>
+			</ApolloProvider>
 		</TailwindProvider>
 	);
 }
