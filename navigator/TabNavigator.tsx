b54fe7c0cbed34b952402093d48abdaf2a5d3848
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-11-26 17:51:52 -0800

    Customers base screen implemented

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from '@rneui/themed';
 import React, { useLayoutEffect } from 'react';
 import CustomerScreen from '../screens/CustomerScreen';
 import OrderScreen from '../screens/OrderScreen';
@@ -21,7 +22,31 @@ const TabNavigator = () => {
 	}, []);
 
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				tabBarActiveTintColor: '#59C1CC',
+				tabBarInactiveTintColor: 'gray',
+				tabBarIcon: ({ focused, color, size }) => {
+					if (route.name === 'Customers') {
+						return (
+							<Icon
+								name="users"
+								type="entypo"
+								color={focused ? '#59C1CC' : 'gray'}
+							/>
+						);
+					} else if (route.name === 'Orders') {
+						return (
+							<Icon
+								name="box"
+								type="entypo"
+								color={focused ? '#EB6A7C' : 'gray'}
+							/>
+						);
+					}
+				},
+			})}
+		>
 			<Tab.Screen name="Customers" component={CustomerScreen} />
 			<Tab.Screen name="Orders" component={OrderScreen} />
 		</Tab.Navigator>
--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,14 +1,47 @@
-import { View, Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import {
+	CompositeNavigationProp,
+	useNavigation,
+} from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Image, Input } from '@rneui/themed';
+import React, { useLayoutEffect, useState } from 'react';
+import { ActivityIndicator, ScrollView } from 'react-native';
 import { useTailwind } from 'tailwind-rn/dist';
+import { RootStack } from '../navigator/RootNavigator';
+import { TabStackParamList } from '../navigator/TabNavigator';
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+	BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+	NativeStackNavigationProp<RootStack>
+>;
 
 const CustomerScreen = () => {
 	const tw = useTailwind();
+	const navigation = useNavigation<CustomerScreenNavigationProp>();
+	const [input, setInput] = useState<string>('');
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerShown: false,
+		});
+	});
 
 	return (
-		<SafeAreaView>
-			<Text style={tw('text-blue-500')}>CustomerScreen</Text>
-		</SafeAreaView>
+		<ScrollView style={{ backgroundColor: '#59C1CC' }}>
+			<Image
+				source={{ uri: 'https://links.papareact.com/3jc' }}
+				containerStyle={tw('w-full h-64')}
+				PlaceholderContent={<ActivityIndicator />}
+			/>
+
+			<Input
+				placeholder="Search by Customer..."
+				value={input}
+				onChangeText={setInput}
+				containerStyle={tw('bg-white pt-5 pb-0 px-10 rounded-lg mt-4')}
+			/>
+		</ScrollView>
 	);
 };
 
